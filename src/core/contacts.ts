b811c391b7c1110b23350ea6335f5ca23d8da440
Markin <Markin-work@protonmail.com>
2025-09-30 18:26:11 -0300

    chore: update informations

--- src/core/contacts.ts
+++ src/core/contacts.ts
@@ -1,15 +1,21 @@
-import { GithubIcon, LinkedinIcon, MailIcon, MapPin } from "lucide-react";
+import { GithubIcon, LinkedinIcon, MailIcon, MapPin, PhoneIcon } from "lucide-react";
+import { info } from "@/core/info";
 
 export const contacts = [
 	{
 		icon: MailIcon,
 		label: "Email",
-		value: "markin-work@protonmail.com",
+		value: info.mail,
 	},
 	{
 		icon: MapPin,
 		label: "Localização",
 		value: "Pernambuco - Brasil",
+	},
+	{
+		icon: PhoneIcon,
+		label: "Telefone",
+		value: info.phone,
 	},
 ];
 
--- src/core/info.ts
+++ src/core/info.ts
@@ -1,4 +1,6 @@
 export const info = {
 	name: "Marcos Lima",
 	age: "2005-03-01T16:44:34+00:00",
+	mail: "markin-work@protonmail.com",
+	phone: "87999631718",
 };
