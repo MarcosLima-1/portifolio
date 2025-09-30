60548d15cb141b9d47395c9dd94d7ac44bfe9200
Markin <Markin-work@protonmail.com>
2025-09-30 18:26:51 -0300

    chore: add more props to title page component

--- src/modules/portifolio/components/title-page.tsx
+++ src/modules/portifolio/components/title-page.tsx
@@ -1,10 +1,13 @@
-interface TitlePageProps {
+import type { ComponentProps } from "react";
+import { cn } from "@/utils/cn";
+
+interface TitlePageProps extends ComponentProps<"div"> {
 	title: string;
 }
 
-export function TitlePage({ title }: TitlePageProps) {
+export function TitlePage({ title, className, ...props }: TitlePageProps) {
 	return (
-		<div className="w-fit">
+		<div className={cn("w-fit", className)} {...props}>
 			<h1 className="mb-4 font-bold text-4xl">{title}</h1>
 			<div className="h-1 w-[70%] rounded-md bg-gradient-to-tr from-primary to-secondary" />
 		</div>
