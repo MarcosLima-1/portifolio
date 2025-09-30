0e2354c33ee1fb6fac650a3c4e8974601e2f6068
Markin <Markin-work@protonmail.com>
2025-09-30 18:27:25 -0300

    fix: removing flick on skill section

--- src/modules/portifolio/pages/about/components/skills-section.tsx
+++ src/modules/portifolio/pages/about/components/skills-section.tsx
@@ -27,7 +27,7 @@ export function SkillsSection() {
 					})}
 				</div>
 
-				<div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:flex-wrap">
+				<div className="grid min-h-[500px] grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:flex lg:flex-wrap">
 					{filteredSkills.map(({ icon, name }) => {
 						return (
 							<Card key={name} className="w-full lg:size-[150px]">
